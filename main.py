ea24f316d4e56a6c25f79a07ac461a43e962bfd9
Andy García <48800899+andygarciach@users.noreply.github.com>
2022-11-17 21:32:57 +0000

    Some code updates regarding to formatting float numbers output

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 ##Tip Calculator
 print("Welcome to the tip calculator")
-total_bill = float(input("What was the total bill? "))
+total_bill = float(input("What was the total bill $? "))
 percentage_tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
 ppl= int(input("How many people to split the bill? "))
-print(f"Each person should pay: {round((total_bill/ppl)*(1+(percentage_tip/100)),2)}")
+bill_per_person = total_bill / ppl
+final_amount = round((total_bill/ppl)*(1+(percentage_tip/100)),2)
+print(f"Each person should pay: {final_amount}")
+final_amount = "{:.2f}".format(bill_per_person)
+print (final_amount)
